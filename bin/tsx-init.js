4e969009c994be4e2818d970edf7a942ed45eaf0
Gregor <gregor.steiner@23degrees.io>
2025-06-05 13:43:58 +0200

    feat: 🔖 1.2.0

--- bin/tsx-init.js
+++ bin/tsx-init.js
@@ -1,12 +1,47 @@
 #!/usr/bin/env node
 
 // src/tsx-init.ts
+var import_node_child_process = require("node:child_process");
 var import_promises = require("node:fs/promises");
+var dependencies = ["std-kit"];
+var devDependencies = ["@biomejs/biome", "@types/node", "esbuild", "tsx", "typescript"];
 var appTsContent = `const runApp = async () => {
   console.log('Hello World');
 }
 
 runApp().catch((error) => console.log('Error', error));`;
+var execPromise = (command) => {
+  return new Promise((resolve, reject) => {
+    (0, import_node_child_process.exec)(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+      }
+      resolve(stdout);
+    });
+  });
+};
+var addYarn = async () => {
+  const yarnCheck = await execPromise("yarn --version").catch(() => null);
+  if (!yarnCheck) return false;
+  await execPromise(`yarn add ${dependencies.join(" ")}`).catch(() => null);
+  await execPromise(`yarn add --dev ${devDependencies.join(" ")}`).catch(() => null);
+  return true;
+};
+var addNpm = async () => {
+  const npmCheck = await execPromise("npm --version").catch(() => null);
+  if (!npmCheck) return false;
+  await execPromise(`npm install ${dependencies.join(" ")}`).catch(() => null);
+  await execPromise(`npm install --save-dev ${devDependencies.join(" ")}`).catch(() => null);
+  return true;
+};
+var addPnpm = async () => {
+  const pnpmCheck = await execPromise("pnpm --version").catch(() => null);
+  if (!pnpmCheck) return false;
+  await execPromise(`pnpm add ${dependencies.join(" ")}`).catch(() => null);
+  await execPromise(`pnpm add --dev ${devDependencies.join(" ")}`).catch(() => null);
+  return true;
+};
 var runApp = async () => {
   const packageJson = await (0, import_promises.readFile)("package.json", "utf-8").then((data) => JSON.parse(data)).catch((error) => console.log("- package.json not found"));
   if (!packageJson) {
@@ -25,12 +60,31 @@ var runApp = async () => {
     packageJson.devDependencies = {};
   }
   await (0, import_promises.writeFile)("package.json", JSON.stringify(packageJson, null, 2));
-  await (0, import_promises.mkdir)("src").then(() => console.log("+ created directory src/")).catch((error) => console.log("- directory src/ already exists"));
-  if (await (0, import_promises.stat)("src/app.ts").catch(() => null)) {
-    console.log("- file src/app.ts already exists");
-  } else {
-    await (0, import_promises.writeFile)("src/app.ts", appTsContent).then(() => console.log("+ created file src/app.ts")).catch((error) => console.log("- error writing src/app.ts", error));
+  const yarnLock = await (0, import_promises.stat)("yarn.lock").catch(() => null);
+  if (yarnLock) {
+    await addYarn();
+    console.log("+ added dependencies with yarn");
   }
-  console.log("> yarn add --dev @types/node esbuild tsx typescript");
+  const npmLock = await (0, import_promises.stat)("package-lock.json").catch(() => null);
+  if (npmLock) {
+    await addNpm();
+    console.log("+ added dependencies with npm");
+  }
+  const pnpmLock = await (0, import_promises.stat)("pnpm-lock.yaml").catch(() => null);
+  if (pnpmLock) {
+    await addPnpm();
+    console.log("+ added dependencies with pnpm");
+  }
+  if (!yarnLock && !npmLock && !pnpmLock) {
+    await addYarn();
+    console.log("+ added dependencies with yarn");
+  }
+  await (0, import_promises.mkdir)("src").then(() => console.log("+ created directory src/")).catch((error) => console.log("- directory src/ already exists"));
+  const appFile = await (0, import_promises.stat)("src/app.ts").catch(() => null);
+  if (!appFile) {
+    await (0, import_promises.writeFile)("src/app.ts", appTsContent).then(() => console.log("+ created file src/app.ts")).catch((error) => console.log("- error writing src/app.ts", error));
+  } else {
+    console.log("- file src/app.ts already exists");
+  }
 };
 runApp().catch((error) => console.log("Error", error));
--- src/tsx-init.ts
+++ src/tsx-init.ts
@@ -1,12 +1,73 @@
 #!/usr/bin/env node
 
+import { exec } from 'node:child_process';
 import { stat, mkdir, readFile, writeFile } from 'node:fs/promises';
+
+const dependencies = ['std-kit'];
+const devDependencies = ['@biomejs/biome', '@types/node', 'esbuild', 'tsx', 'typescript'];
 
 const appTsContent = `const runApp = async () => {
   console.log('Hello World');
 }
 
 runApp().catch((error) => console.log('Error', error));`;
+
+const execPromise = (command: string) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+      }
+
+      // console.log(`stdout: ${stdout}`);
+      // console.error(`stderr: ${stderr}`);
+      resolve(stdout);
+    });
+  });
+};
+
+const addYarn = async () => {
+  // check if yarn is installed
+  const yarnCheck = await execPromise('yarn --version').catch(() => null);
+  if (!yarnCheck) return false;
+
+  // install dependencies
+  await execPromise(`yarn add ${dependencies.join(' ')}`).catch(() => null);
+
+  // install devDependencies
+  await execPromise(`yarn add --dev ${devDependencies.join(' ')}`).catch(() => null);
+
+  return true;
+};
+
+const addNpm = async () => {
+  // check if npm is installed
+  const npmCheck = await execPromise('npm --version').catch(() => null);
+  if (!npmCheck) return false;
+
+  // install dependencies
+  await execPromise(`npm install ${dependencies.join(' ')}`).catch(() => null);
+
+  // install devDependencies
+  await execPromise(`npm install --save-dev ${devDependencies.join(' ')}`).catch(() => null);
+
+  return true;
+};
+
+const addPnpm = async () => {
+  // check if pnpm is installed
+  const pnpmCheck = await execPromise('pnpm --version').catch(() => null);
+  if (!pnpmCheck) return false;
+
+  // install dependencies
+  await execPromise(`pnpm add ${dependencies.join(' ')}`).catch(() => null);
+
+  // install devDependencies
+  await execPromise(`pnpm add --dev ${devDependencies.join(' ')}`).catch(() => null);
+
+  return true;
+};
 
 const runApp = async () => {
   // read package.json
@@ -39,21 +100,44 @@ const runApp = async () => {
 
   await writeFile('package.json', JSON.stringify(packageJson, null, 2));
 
+  // check if yarn.lock exists
+  const yarnLock = await stat('yarn.lock').catch(() => null);
+  if (yarnLock) {
+    await addYarn();
+    console.log('+ added dependencies with yarn');
+  }
+
+  const npmLock = await stat('package-lock.json').catch(() => null);
+  if (npmLock) {
+    await addNpm();
+    console.log('+ added dependencies with npm');
+  }
+
+  const pnpmLock = await stat('pnpm-lock.yaml').catch(() => null);
+  if (pnpmLock) {
+    await addPnpm();
+    console.log('+ added dependencies with pnpm');
+  }
+
+  if (!yarnLock && !npmLock && !pnpmLock) {
+    await addYarn();
+    console.log('+ added dependencies with yarn');
+  }
+
   // create src directory
   await mkdir('src')
     .then(() => console.log('+ created directory src/'))
     .catch((error) => console.log('- directory src/ already exists'));
 
-  // create src/app.ts
-  if (await stat('src/app.ts').catch(() => null)) {
-    console.log('- file src/app.ts already exists');
-  } else {
+  // create src/app.ts file
+  const appFile = await stat('src/app.ts').catch(() => null);
+  if (!appFile) {
     await writeFile('src/app.ts', appTsContent)
       .then(() => console.log('+ created file src/app.ts'))
       .catch((error) => console.log('- error writing src/app.ts', error));
+  } else {
+    console.log('- file src/app.ts already exists');
   }
-
-  console.log('> yarn add --dev @types/node esbuild tsx typescript');
 };
 
 runApp().catch((error) => console.log('Error', error));
